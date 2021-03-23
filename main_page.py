155190bc6afa5bba900e2bbb80263ba021abd5a6
PC-MAX\Max <max.lutz@edu.ece.fr>
2021-03-23 01:24:19 +0100

    first test on the vizualisation page

--- main_page.py
+++ main_page.py
@@ -5,64 +5,46 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import numpy as np
 
-st.title('NBA Player Stats Explorer')
+st.title('French national assembly vizualisation tool')
 
 st.markdown("""
-This app performs simple webscraping of NBA player stats data!
+This app performs simple vizualisation from the open data from the french national assembly!
 * **Python libraries:** base64, pandas, streamlit
-* **Data source:** [Basketball-reference.com](https://www.basketball-reference.com/).
+* **Data source:** [national assembly open data](https://data.assemblee-nationale.fr/).
 """)
 
 st.sidebar.header('User Input Features')
-selected_year = st.sidebar.selectbox('Year', list(reversed(range(1950,2020))))
 
-# Web scraping of NBA player stats
+deputies = pd.read_csv('df_dep.csv')
+
 @st.cache
-def load_data(year):
-    url = "https://www.basketball-reference.com/leagues/NBA_" + str(year) + "_per_game.html"
-    html = pd.read_html(url, header = 0)
-    df = html[0]
-    raw = df.drop(df[df.Age == 'Age'].index) # Deletes repeating headers in content
-    raw = raw.fillna(0)
-    playerstats = raw.drop(['Rk'], axis=1)
-    return playerstats
-playerstats = load_data(selected_year)
+def get_data():
+    return pd.read_csv('df_dep.csv')
+deputies = get_data()
 
-# Sidebar - Team selection
-sorted_unique_team = sorted(playerstats.Tm.unique())
-selected_team = st.sidebar.multiselect('Team', sorted_unique_team, sorted_unique_team)
+pol_parties = deputies["pol party"].unique().tolist()
+pol_party_selected = st.sidebar.multiselect('Political parties', pol_parties, pol_parties)
+sex = deputies["sex"].unique().tolist()
+sex_selected = st.sidebar.multiselect('Sex', sex, sex) 
+jobs = deputies["activity"].unique().tolist()
+jobs_selected = st.sidebar.multiselect('Political parties', jobs, jobs) 
+#jobs = df['activity'].drop_duplicates()
+#job_choice = st.sidebar.selectbox('Jobs:', jobs)
 
-# Sidebar - Position selection
-unique_pos = ['C','PF','SF','PG','SG']
-selected_pos = st.sidebar.multiselect('Position', unique_pos, unique_pos)
+mask_pol_parties = deputies['pol party'].isin(pol_party_selected)
+mask_sex = deputies['sex'].isin(sex_selected)
+mask_jobs = deputies['activity'].isin(jobs_selected)
 
-# Filtering data
-df_selected_team = playerstats[(playerstats.Tm.isin(selected_team)) & (playerstats.Pos.isin(selected_pos))]
+display_df = deputies[mask_pol_parties & mask_sex & mask_jobs]
+display_df = display_df.sort_values(by=['pol party'])
+# Displays the user input features
+st.subheader('User Input features')
 
-st.header('Display Player Stats of Selected Team(s)')
-st.write('Data Dimension: ' + str(df_selected_team.shape[0]) + ' rows and ' + str(df_selected_team.shape[1]) + ' columns.')
-st.dataframe(df_selected_team)
+st.subheader('Dataframe')
+st.write(display_df)
 
-# Download NBA player stats data
-# https://discuss.streamlit.io/t/how-to-download-file-in-streamlit/1806
-def filedownload(df):
-    csv = df.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # strings <-> bytes conversions
-    href = f'<a href="data:file/csv;base64,{b64}" download="playerstats.csv">Download CSV File</a>'
-    return href
+st.write(display_df['pol party'].unique().tolist())
+st.write(display_df['pol party'].value_counts(sort=False))
 
-st.markdown(filedownload(df_selected_team), unsafe_allow_html=True)
-
-# Heatmap
-if st.button('Intercorrelation Heatmap'):
-    st.header('Intercorrelation Matrix Heatmap')
-    df_selected_team.to_csv('output.csv',index=False)
-    df = pd.read_csv('output.csv')
-
-    corr = df.corr()
-    mask = np.zeros_like(corr)
-    mask[np.triu_indices_from(mask)] = True
-    with sns.axes_style("white"):
-        f, ax = plt.subplots(figsize=(7, 5))
-        ax = sns.heatmap(corr, mask=mask, vmax=1, square=True)
-    st.pyplot()
+plt.pie(display_df['pol party'].value_counts(sort=False), labels=display_df['pol party'].unique().tolist())
+st.pyplot()
